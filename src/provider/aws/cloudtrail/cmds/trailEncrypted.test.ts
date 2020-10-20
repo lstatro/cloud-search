05e4bbc89f74c6a3d2a727084faf86cce873506d
lst <jdhoffmeyer@gmail.com>
2020-10-20 18:54:42 -0400

    trailEncrypted should have 100% code coverage

--- src/provider/aws/cloudtrail/cmds/trailEncrypted.test.ts
+++ src/provider/aws/cloudtrail/cmds/trailEncrypted.test.ts
@@ -77,4 +77,119 @@ describe('cloudtrail events must be configured correctly', () => {
       },
     ])
   })
+
+  it('should return UNKNOWN if there is no trail metadata', async () => {
+    mock('CloudTrail', 'listTrails', {
+      Trails: [
+        {
+          TrailARN: 'test',
+          HomeRegion: 'us-east-1',
+        },
+      ],
+    })
+
+    mock('CloudTrail', 'getTrail', {})
+
+    mock('KMS', 'describeKey', {
+      KeyMetadata: { Arn: 'test', KeyManager: 'AWS' },
+    })
+
+    const audits = await handler({
+      region: 'all',
+      keyType: 'aws',
+      profile: 'test',
+    })
+
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'us-east-1',
+        rule: 'TrailEncrypted',
+        service: 'cloudtrail',
+        state: 'UNKNOWN',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
+  })
+
+  it('should return FAIL if there is no key in the metadata', async () => {
+    mock('CloudTrail', 'listTrails', {
+      Trails: [
+        {
+          TrailARN: 'test',
+          HomeRegion: 'us-east-1',
+        },
+      ],
+    })
+
+    mock('CloudTrail', 'getTrail', {
+      Trail: {},
+    })
+
+    mock('KMS', 'describeKey', {
+      KeyMetadata: { Arn: 'test', KeyManager: 'AWS' },
+    })
+
+    const audits = await handler({
+      region: 'all',
+      keyType: 'aws',
+      profile: 'test',
+    })
+
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'us-east-1',
+        rule: 'TrailEncrypted',
+        service: 'cloudtrail',
+        state: 'FAIL',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
+  })
+
+  it('should return WARNING if there is a AWS managed key when looking for a cmk', async () => {
+    mock('CloudTrail', 'listTrails', {
+      Trails: [
+        {
+          TrailARN: 'test',
+          HomeRegion: 'us-east-1',
+        },
+      ],
+    })
+
+    mock('CloudTrail', 'getTrail', {
+      Trail: {
+        KmsKeyId: 'test',
+      },
+    })
+
+    mock('KMS', 'describeKey', {
+      KeyMetadata: { Arn: 'test', KeyManager: 'AWS' },
+    })
+
+    const audits = await handler({
+      region: 'us-east-1',
+      resourceId: 'test',
+      keyType: 'cmk',
+      profile: 'test',
+    })
+
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'us-east-1',
+        rule: 'TrailEncrypted',
+        service: 'cloudtrail',
+        state: 'WARNING',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
+  })
 })
