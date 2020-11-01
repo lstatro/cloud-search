e2ac41b25a25d1fdbe4581400cedcc9518a9402c
kuzirh <hrizukmichael@gmail.com>
2020-11-01 18:37:02 -0500

    cloud search should have service to audit neptune instances, with passing tests and 100% coverage

--- src/provider/aws/neptune/neptune.ts
+++ src/provider/aws/neptune/neptune.ts
@@ -8,16 +8,14 @@ const opts = {
 }
 
 export const builder = (yargs: Argv) => {
-  return (
-    yargs
-      .commandDir('clusters', opts)
-      // .commandDir('instances', opts)
-      .demandCommand(1)
-      .example([
-        [
-          'neptune clusters <rule>...',
-          'have the cli show you available cluster rule',
-        ],
-      ])
-  )
+  return yargs
+    .commandDir('clusters', opts)
+    .commandDir('instances', opts)
+    .demandCommand(1)
+    .example([
+      [
+        'neptune clusters <rule>...',
+        'have the cli show you available cluster rule',
+      ],
+    ])
 }
