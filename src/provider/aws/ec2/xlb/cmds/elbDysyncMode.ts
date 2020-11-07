69415197d29c260671de3c2e2d077949dff1e058
lst <jdhoffmeyer@gmail.com>
2020-11-06 21:41:35 -0500

    elbs should have desync mode set to strict

--- src/provider/aws/ec2/xlb/cmds/elbDysyncMode.ts
+++ src/provider/aws/ec2/xlb/cmds/elbDysyncMode.ts
@@ -3,22 +3,22 @@ import { AWS } from '../../../../../lib/aws/AWS'
 import assert from 'assert'
 import { LoadBalancerDescription } from 'aws-sdk/clients/elb'
 
-const rule = 'ElbAccessLogsEnabled'
+const rule = 'ElbDesyncMode'
 
 export const command = `${rule} [args]`
-export const desc = `Verifies classic elastic load balancers has access logging 
+export const desc = `Verifies classic elastic load balancer's desync mode 
 enabled
 
-  OK      - LB has logging enabled
-  WARNING - LB has logging enabled but does not have a target s3 bucket
-  UNKNOWN - Unable to determine if LB has logging enabled
-  FAIL    - LB does not have logging enabled
+  OK      - LB is set to strict mode
+  WARNING - LB is set to defensive mode
+  UNKNOWN - Unable to determine LB'er desync mode
+  FAIL    - LB is set to monitor or does not have the mode enabled
 
   resourceId - load balancer name
 
 `
 
-export default class ElbAccessLogsEnabled extends AWS {
+export default class ElbDesyncMode extends AWS {
   audits: AuditResultInterface[] = []
   service = 'ec2'
 
@@ -51,17 +51,34 @@ export default class ElbAccessLogsEnabled extends AWS {
       region: region,
     })
 
+    let mode = 'monitor'
+
     if (attributes) {
-      if (attributes.AccessLog) {
-        if (attributes.AccessLog.Enabled === true) {
-          audit.state = 'WARNING'
-          if (typeof attributes.AccessLog.S3BucketName === 'string') {
-            audit.state = 'OK'
+      if (attributes.AdditionalAttributes) {
+        for (const attribute of attributes.AdditionalAttributes) {
+          if (attribute.Key) {
+            if (attribute.Key.includes('desyncmitigationmode')) {
+              if (attribute.Value) {
+                mode = attribute.Value
+              }
+            }
           }
-        } else {
-          audit.state = 'FAIL'
         }
       }
+    }
+
+    const modeMap: { [key: string]: 'OK' | 'WARNING' | 'FAIL' } = {
+      strictest: 'OK',
+      defensive: 'WARNING',
+      monitor: 'FAIL',
+    }
+
+    if (mode) {
+      if (modeMap[mode]) {
+        audit.state = modeMap[mode]
+      }
+    } else {
+      audit.state = 'FAIL'
     }
 
     this.audits.push(audit)
@@ -101,7 +118,7 @@ export default class ElbAccessLogsEnabled extends AWS {
 }
 
 export const handler = async (args: AWSScannerInterface) => {
-  const scanner = await new ElbAccessLogsEnabled(args)
+  const scanner = await new ElbDesyncMode(args)
   await scanner.start()
   scanner.output()
   return scanner.audits
