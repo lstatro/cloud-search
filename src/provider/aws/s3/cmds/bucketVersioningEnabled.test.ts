7a5e80d51831ccb3a5fb59a13b4f9c56a29bb194
kuzirh <hrizukmichael@gmail.com>
2020-11-25 18:45:36 -0500

    s3 bucket versioning check should have audits and tests with 100% coverage

--- src/provider/aws/s3/cmds/bucketVersioningEnabled.test.ts
+++ src/provider/aws/s3/cmds/bucketVersioningEnabled.test.ts
@@ -17,13 +17,87 @@ describe('S3 Bucket Versioning Enabled', () => {
     clock.restore()
     restore()
   })
-  it('should list all buckets in an account, and audit each', async () => {
-    mock('EC2', 'listBuckets', [{ Name: 'test' }])
+  it('should return empty audit if no buckets found', async () => {
+    mock('S3', 'listBuckets', {
+      Buckets: [],
+    })
+    const audits = await handler({
+      region: 'test',
+      profile: 'test',
+    })
+    expect(audits).to.eql([])
+  })
+
+  it('should return OK if versioning is enabled', async () => {
+    mock('S3', 'listBuckets', {
+      Buckets: [
+        {
+          Name: 'test',
+        },
+      ],
+    })
+    mock('S3', 'getBucketVersioning', { Status: 'Enabled' })
+    const audits = await handler({
+      region: 'test',
+      profile: 'test',
+    })
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'test',
+        rule: 'BucketVersioningEnabled',
+        service: 's3',
+        state: 'OK',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
+  })
+  it('should return fail if versioning is suspended', async () => {
+    mock('S3', 'listBuckets', {
+      Buckets: [
+        {
+          Name: 'test',
+        },
+      ],
+    })
+    mock('S3', 'getBucketVersioning', { Status: 'Suspended' })
+    const audits = await handler({
+      region: 'test',
+      profile: 'test',
+    })
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'test',
+        rule: 'BucketVersioningEnabled',
+        service: 's3',
+        state: 'FAIL',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
+  })
+  it('should return an audit when a resourceId is passed in', async () => {
+    mock('S3', 'getBucketVersioning', { Status: 'Enabled' })
     const audits = await handler({
       region: 'test',
       profile: 'test',
       resourceId: 'test',
     })
-    expect(audits).to.eql({})
+    expect(audits).to.eql([
+      {
+        physicalId: 'test',
+        profile: 'test',
+        provider: 'aws',
+        region: 'test',
+        rule: 'BucketVersioningEnabled',
+        service: 's3',
+        state: 'OK',
+        time: '1970-01-01T00:00:00.000Z',
+      },
+    ])
   })
 })
--- src/provider/aws/s3/cmds/bucketVersioningEnabled.ts
+++ src/provider/aws/s3/cmds/bucketVersioningEnabled.ts
@@ -2,9 +2,9 @@ import {
   AuditResultInterface,
   AWSScannerInterface,
 } from '@lstatro/cloud-search'
-import { Bucket } from 'aws-sdk/clients/cloudsearchdomain'
+import { Bucket } from 'aws-sdk/clients/s3'
 import { AWS } from '../../../../lib/aws/AWS'
-
+import assert from 'assert'
 const rule = 'BucketVersioningEnabled'
 
 export const command = `${rule} [args]`
@@ -30,21 +30,45 @@ export default class BucketVersioningEnabled extends AWS {
 
   async audit({ resource, region }: { resource: string; region: string }) {
     const options = this.getOptions()
+    const s3 = new this.AWS.S3(options)
     options.region = region
     const audit = this.getDefaultAuditObj({
       resource: resource,
       region,
     })
+    const getBucketVersioning = await s3
+      .getBucketVersioning({ Bucket: resource })
+      .promise()
+    const isVersioning = getBucketVersioning.Status === 'Enabled'
+    if (isVersioning) {
+      audit.state = 'OK'
+    } else {
+      audit.state = 'FAIL'
+    }
+
+    this.audits.push(audit)
   }
 
-  scan = async () => {
-    console.log('scan')
-    const options = this.getOptions()
-    const promise = new this.AWS.S3(options).listBuckets().promise()
-    const buckets = await this.pager<Bucket>(promise, 'Buckets')
-    for (const bucket of buckets) {
-      assert(bucket.Name, 'bucket does not have a name')
-      this.audit({ resource: bucket.Name, region })
+  scan = async ({
+    resourceId,
+    region,
+  }: {
+    resourceId: string
+    region: string
+  }) => {
+    if (resourceId) {
+      await this.audit({ resource: resourceId, region })
+    } else {
+      const options = this.getOptions()
+      options.region = region
+
+      const promise = new this.AWS.S3(options).listBuckets().promise()
+      const buckets = await this.pager<Bucket>(promise, 'Buckets')
+
+      for (const bucket of buckets) {
+        assert(bucket.Name, 'the bucket must have a name')
+        await this.audit({ resource: bucket.Name, region })
+      }
     }
   }
 }
