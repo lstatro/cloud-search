8d3c2b6dddd381e6f0098c6f7ee016a480ad4e30
lst <jdhoffmeyer@gmail.com>
2020-11-25 09:57:13 -0500

    refactored emr, added loggingEnabled

--- src/provider/aws/emr/cmds/securityConfiguration.ts
+++ src/provider/aws/emr/cmds/securityConfiguration.ts
@@ -1,9 +1,5 @@
-import {
-  AuditResultInterface,
-  AWSScannerInterface,
-} from '@lstatro/cloud-search'
-import assert from 'assert'
-import { AWS } from '../../../../lib/aws/AWS'
+import { AWSScannerInterface } from '@lstatro/cloud-search'
+import { EmrCluster } from './emrCluster'
 
 const rule = 'SecurityConfiguration'
 
@@ -18,90 +14,14 @@ configuration group
 
   resourceId - cluster ID
 
-  note: Scans only look at active clusters.  It's still possible to manually
-        audit terminated instances by passing in the terminated resource id in
-        resource scan (... emr -r us-east-1 -i j-XXXXX )
-
-        valid scan states are:
-          - STARTING
-          - BOOTSTRAPPING
-          - RUNNING
-          - WAITING
-          - TERMINATING
-
 `
 
-export class SecurityConfiguration extends AWS {
-  audits: AuditResultInterface[] = []
-  service = 'emr'
-  global = false
-
-  constructor(public params: AWSScannerInterface) {
-    super({ ...params, rule })
-  }
-
-  async audit({ resource, region }: { resource: string; region: string }) {
-    const options = this.getOptions()
-    options.region = region
-
-    assert(resource, 'emr cluster id is required')
-
-    const audit = this.getDefaultAuditObj({ resource: resource, region })
-
-    const describeCluster = await new this.AWS.EMR(options)
-      .describeCluster({
-        ClusterId: resource,
-      })
-      .promise()
-
-    if (describeCluster.Cluster) {
-      if (describeCluster.Cluster.SecurityConfiguration) {
-        audit.state = 'OK'
-      } else {
-        audit.state = 'FAIL'
-      }
-    }
-
-    this.audits.push(audit)
-  }
-
-  scan = async ({
-    resourceId,
-    region,
-  }: {
-    resourceId: string
-    region: string
-  }) => {
-    const options = this.getOptions()
-    options.region = region
-
-    if (resourceId) {
-      await this.audit({ resource: resourceId, region })
-    } else {
-      const listClusters = await new this.AWS.EMR(options)
-        .listClusters({
-          ClusterStates: [
-            'STARTING',
-            'BOOTSTRAPPING',
-            'RUNNING',
-            'WAITING',
-            'TERMINATING',
-          ],
-        })
-        .promise()
-
-      if (listClusters.Clusters) {
-        for (const cluster of listClusters.Clusters) {
-          assert(cluster.Id, 'cluster ID not found')
-          await this.audit({ resource: cluster.Id, region })
-        }
-      }
-    }
-  }
-}
-
 export const handler = async (args: AWSScannerInterface) => {
-  const scanner = new SecurityConfiguration(args)
+  const scanner = new EmrCluster({
+    ...args,
+    rule,
+    attribute: 'SecurityConfiguration',
+  })
 
   await scanner.start()
   scanner.output()
--- src/provider/aws/emr/emr.ts
+++ src/provider/aws/emr/emr.ts
@@ -7,6 +7,21 @@ const opts = {
   exclude: /.*.test.js/,
 }
 
+const epilog = `
+**************************** Instance States ***********************************
+
+Scans only look at active clusters.  It's still possible to manually
+audit terminated instances by passing in the terminated resource id in
+resource scan (... emr -r us-east-1 -i j-XXXXX )
+
+valid states are:
+  - STARTING
+  - BOOTSTRAPPING
+  - RUNNING
+  - WAITING
+  - TERMINATING
+`
+
 export const builder = (yargs: Argv) => {
-  return yargs.commandDir('cmds', opts).demandCommand(1)
+  return yargs.commandDir('cmds', opts).demandCommand(1).epilog(epilog)
 }
